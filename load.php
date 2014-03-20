876d8ba1f174a4017ecc1d13be3220d7012b0950
Josh <josh@kaiju.net>
2014-03-19 22:06:36 -0400

    Removed load.php

--- load.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (isset($_GET['filename']) && file_exists(basename($_GET['filename']))) {
-    header("Content-type: text/plain; charset=ISO-8859-1");
-    echo file_get_contents(basename($_GET['filename']));
-} else {
-    header("HTTP/1.0 404 Not Found");
-}
-?>
