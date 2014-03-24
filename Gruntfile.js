8b39ba9afc6005ab88cabdc968c55df7f110aad5
Josh <josh@kaiju.net>
2014-03-23 21:49:05 -0400

    Added initial grunt files to keep up with the cool kids

---
+++ Gruntfile.js
@@ -0,0 +1,39 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		uglify: {
+			options: {
+				compress: {
+					drop_console: true
+				},
+				banner: "(function() {",
+				footer: "})()"
+			},
+			dist: {
+				files: {
+					"dist/ansi-min.js": ['src/*.js']
+				}
+			}
+		},
+		concat: {
+			dist: {
+				src: [
+					'src/ansi.js',
+					'src/ansi_parser.js',
+					'src/textmode.js',
+					'src/characterset-cp437.js'
+				],
+				dest: 'dist/ansi.js',
+				options: {
+					banner: "(function() {\r\n",
+					footer: "})()"
+				}
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+
+}
